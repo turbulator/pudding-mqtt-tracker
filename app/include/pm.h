decdf00e05ed6dc60e64d9cf2fe0bf2ad8a4b585
Alexey Vlasov <mail4turbo@gmail.com>
2020-03-26 17:22:04 +0700

    A9G: add PM module

---
+++ app/include/pm.h
@@ -0,0 +1,7 @@
+#ifndef __PM_H_
+#define __PM_H_
+
+#define 
+
+
+#endif /* __PM_H_ */
---
+++ app/src/pm.c
@@ -0,0 +1,11 @@
+#include <api_os.h>
+#include <api_hal_gpio.h>
+#include <api_gps.h>
+
+#include "pm.h"
+
+
+void PmInit(void){
+}
+
+
